1ef9d93c2f9b6ea1ae89e9bd827294bf5c749baa
hocinebens <36114451+hocinebens@users.noreply.github.com>
2018-11-09 08:47:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<title> Our</title>
+		<link rel="stylesheet" href="css/styles.css" />
+		
+	</head>
+
+	<body>
+	<ul id="nav">
+
+		<li><a href="index">home</a></li>
+		<li><a href="about">about us</a></li>
+		<li><a href="contact">contact</a></li>
+		<li><a href="bein-sport-1">bein-sport-1</a></li>
+		<li><a href="bein-sport-2">bein-sport-2</a></li>
+		<li><a href="bein-sport-3">bein-sport-3</a></li>
+		<li><a href="bein-sport-4">bein-sport-4</a></li>
+		<li><a href="bein-sport-5">bein-sport-5</a></li>
+	</ul>
+	<div id="content"></div>
+	<script src="http://code.jquery.com/jquery-1.7.1.js"></script>
+	<script src="js/general.js"></script>
+	</body>
+</html>
